c461fba2f7a5379049b1f46938a822ea8aede5c2
limadouglas07 <lima.douglas08@gmail.com>
2024-04-25 00:11:22 -0300

    Update App.java
    adicionada a linha 4

--- primeiro-projeto-java/src/App.java
+++ primeiro-projeto-java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas Vindas Turma Dio!!!");
+        System.out.println("Acabei de adicionar essa linha");
     }
 }
