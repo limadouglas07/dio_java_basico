5f9a3dfe258f02e523c160499f5148b088221599
limadouglas07 <lima.douglas08@gmail.com>
2024-04-25 19:25:47 -0300

    Create MinhaClasse.java

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Douglas";
+        String segundoNome = "Lima";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
