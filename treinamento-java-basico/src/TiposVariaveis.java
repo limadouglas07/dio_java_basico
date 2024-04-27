ca59a4f42791270ea84179a8da1211cd4e021516
limadouglas07 <lima.douglas08@gmail.com>
2024-04-27 17:19:45 -0300

    Update TiposVariaveis.java

--- treinamento-java-basico/src/TiposVariaveis.java
+++ treinamento-java-basico/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; /*Por ser uma constante deve conter a palavra final e ser escrita a variável em maiúscula */        
+        System.out.println(VALOR_DE_PI);
+
+        int n = 5; 
+        n = - n;  /* Operador unário serve para inverter o sinal */
+        System.out.println(n); 
+
+        n = n * -1; /*Para transformar em número positivo */
+        System.out.println(n);
+        
+
+
+
+        
+
     }
 }
